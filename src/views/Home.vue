c708768c7a6a8d528993f08e8af71b371f86625d
lbertoncello <lucas_b_andrade@hotmail.com>
2022-01-19 14:56:28 -0300

    refactor: remove unnecessary things

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis aliquid doloremque aut doloribus eos similique. Sed aperiam modi optio illo corrupti id error rerum quae quasi iure tempore sint voluptatem nemo corporis consequatur perspiciatis, reprehenderit inventore! Inventore veniam eveniet odit ullam voluptatum eligendi nemo delectus soluta optio nostrum excepturi ea praesentium exercitationem et culpa, adipisci fuga reprehenderit aspernatur, illo vel itaque quisquam, corporis iusto! Voluptatum laborum voluptas reprehenderit, commodi dolor nulla facere. Et libero voluptatum ex temporibus beatae quo perspiciatis reprehenderit maxime harum necessitatibus accusamus, optio ullam unde sequi quia tempora assumenda. Ipsam nihil molestiae provident reiciendis beatae possimus perspiciatis.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis aliquid doloremque aut doloribus eos similique. Sed aperiam modi optio illo corrupti id error rerum quae quasi iure tempore sint voluptatem nemo corporis consequatur perspiciatis, reprehenderit inventore! Inventore veniam eveniet odit ullam voluptatum eligendi nemo delectus soluta optio nostrum excepturi ea praesentium exercitationem et culpa, adipisci fuga reprehenderit aspernatur, illo vel itaque quisquam, corporis iusto! Voluptatum laborum voluptas reprehenderit, commodi dolor nulla facere. Et libero voluptatum ex temporibus beatae quo perspiciatis reprehenderit maxime harum necessitatibus accusamus, optio ullam unde sequi quia tempora assumenda. Ipsam nihil molestiae provident reiciendis beatae possimus perspiciatis.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: { }
 }
 </script>
