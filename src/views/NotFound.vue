66e13b4d4f313e582ae6c9166be45aa7931ee07b
lbertoncello <lucas_b_andrade@hotmail.com>
2022-01-20 15:54:57 -0300

    feat(NotFound): add styles

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,4 +1,18 @@
 <template>
-  <h2>404</h2>
-  <h3>Page not found</h3>
+  <h2 class="error-code">404</h2>
+  <h3 class="error-msg">Page not found</h3>
 </template>
+
+<style>
+  h2.error-code {
+    font-size: 8rem;
+    color: #969fa8;
+    margin: 5rem auto 2rem;
+  }
+
+  h3.error-msg {
+    font-size: 2rem;
+    text-transform: uppercase;
+    color: #969fa8;
+  }
+</style>
